46ee88a437b6d72482b4663e197ed24417bfdd6a
Fisher48 <rybakov48@yandex.ru>
2024-03-21 20:43:13 +0300

    Created Squirrel.java

---
+++ Squirrel.java
@@ -0,0 +1,18 @@
+public class Squirrel {
+    public static int squirrel(int N) {
+
+        if (N < 0) {
+            throw new IllegalArgumentException("Number is negative: " + N);
+        }
+
+        int e = 1;
+        for (int i = N; i > 0; i--) {
+            assert (e > 0) : "Out of range for int";
+            e *= N--;
+        }
+        while (e > 10) {
+            e = e / 10;
+        }
+        return e;
+    }
+}
