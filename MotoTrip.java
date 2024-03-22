8f687581fb775c5b77eee97a18be3b5de3d375b9
Fisher48 <rybakov48@yandex.ru>
2024-03-22 22:23:09 +0300

    Created MotoTrip

---
+++ MotoTrip.java
@@ -0,0 +1,22 @@
+public class MotoTrip {
+
+    public static int odometer(int[] oksana) {
+        int dist = 0;
+        int temp = 0;
+        if (oksana.length < 2) {
+            throw new IllegalArgumentException("Array size should be >= 2: " + "but current size: " + oksana.length);
+        }
+        for (int i = 0; i < oksana.length - 1; i++) {
+            if (oksana[i] < 0) {
+                throw new IllegalArgumentException("Speed or time is negative: " + oksana[i]);
+            }
+            if (i % 2 == 0) {
+                dist = oksana[i] * oksana[i + 1] - temp;
+                temp = dist;
+            }
+        }
+        return dist;
+    }
+}
+
+
