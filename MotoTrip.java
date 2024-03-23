23f855bcea04d1c1319fbb4eb9532e0dd2b711e9
Fisher48 <rybakov48@yandex.ru>
2024-03-23 18:29:02 +0300

    Fixing after tested

--- MotoTrip.java
+++ MotoTrip.java
@@ -3,17 +3,17 @@ import java.util.*;
 public class Level1 {
     public static int odometer(int[] oksana) {
         int dist = 0;
-        int temp = 0;
         if (oksana.length < 2) {
             throw new IllegalArgumentException("Array size should be >= 2: " + "but current size: " + oksana.length);
         }
-        for (int i = 0; i < oksana.length - 1; i++) {
+        for (int i = 0; i < oksana.length; i++) {
             if (oksana[i] < 0) {
                 throw new IllegalArgumentException("Speed or time is negative: " + oksana[i]);
             }
-            if (i % 2 == 0) {
-                dist = oksana[i] * oksana[i + 1] - temp;
-                temp = dist;
+            if (i % 2 == 0 && i > 0) {
+                dist += oksana[i] * (oksana[i + 1] - oksana[i - 1]);
+            } else if (i == 0) {
+                dist = oksana[i] * oksana[i + 1];
             }
         }
         return dist;
