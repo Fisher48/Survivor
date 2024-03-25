b2262d8832407bb6ab3ae322ecba53ebc99d4b8c
Fisher48 <rybakov48@yandex.ru>
2024-03-25 18:14:15 +0300

    Added RestoreSalarySheet

---
+++ RestoreSalarySheet.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Level1 {
+   public static int [] SynchronizingTables(int N, int [] ids, int [] salary) {
+       int sheet[] = new int[N];
+       int copyIds[] = new int[N];
+       for (int i = 0; i < N; i++) {
+           copyIds[i] = ids[i];
+       }
+       Arrays.sort(salary);
+       Arrays.sort(copyIds);
+           for (int i = 0; i < N; i++) {
+               for (int j = 0; j < N; j++) {
+                   if (copyIds[i] == ids[j]) {
+                       sheet[j] = salary[i];
+                       break;
+                   }
+               }
+           }
+       return sheet;
+    }
+}
+
+
