a3334fc35972c6c5f942eb892c55288206814042
Fisher48 <rybakov48@yandex.ru>
2024-03-27 19:29:24 +0300

    Added GoogleSearch.java

---
+++ GoogleSearch.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Level1 {
+    public static int[] WordSearch(int len, String s, String subs) {
+        ArrayList<String> list = new ArrayList<>();
+        int countW = 0;
+        String[] firstString = s.split(" ");
+        for (int i = 0; i < firstString.length; i++) {
+            if (firstString[i] == "") {
+                continue;
+            }
+            if (i < firstString.length - 1) {
+                if (firstString[i].length() + firstString[i + 1].length() <= len) {
+                    list.add(firstString[i] + " " + firstString[i + 1]);
+                    i++;
+                    continue;
+                }
+                if (firstString[i].length() + firstString[i + 1].length() > len) {
+                    list.add(firstString[i]);
+                }
+            } else if (i > 0) {
+                list.add(firstString[i]);
+            }
+        }
+        int[] FindWord = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            String[] check = list.get(i).split(" ");
+            for (int j = 0; j < check.length; j++) {
+                String s1 = check[j];
+                if (s1.equals(subs)) {
+                    FindWord[i]++;
+                }
+            }
+        }
+        return FindWord;
+    }
+}
+
+
