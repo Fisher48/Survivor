48fe664cfdc244d4f6cb6c5291400adf736606c6
Fisher48 <rybakov48@yandex.ru>
2024-04-05 16:39:45 +0300

    Added UfoSignals

---
+++ UfoSignals.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class UfoSignals {
+   public static int [] UFO(int N, int [] data, boolean octal) {
+
+       int[] decimalCode = new int[N];
+       int temp = 0;
+       int numb = 0;
+       int extent = 0;
+       int ratio = 0;
+       int currentNumb = 0;
+
+       if (!octal) {
+           ratio = 16;
+       } else if (octal) {
+           ratio = 8;
+       }
+       int count = 0;
+
+       for (int j = 0; j < N; j++) {
+           currentNumb = data[j];
+           temp = currentNumb;
+           while (currentNumb > 0) {
+               count++;
+               currentNumb /= 10;
+           }
+           int[] digits = new int[count];
+           currentNumb = data[j];
+           for (int k = 0; k < count; k++) {
+               temp %= 10;
+               currentNumb /= 10;
+               digits[k] = temp;
+               temp = currentNumb;
+           }
+           for (int i = 0; i < count; i++) {
+               extent = (int) Math.pow(ratio, i);
+               numb += extent * digits[i];
+           }
+           decimalCode[j] = numb;
+           count = 0;
+           numb = 0;
+       }
+       return decimalCode;
+   }
+}
+
+
