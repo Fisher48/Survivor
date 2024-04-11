0ff591b4c311610b3c55bf36cc9f9edd1e9d981f
Fisher48 <rybakov48@yandex.ru>
2024-04-11 15:05:18 +0300

    Added TankRushTest

---
+++ TankRushTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TankRushTest {
+
+    @Test
+    public void SampleTest () {
+        String S1 = "3 4 1234 2345 0987";
+        String S2 = "2 2 34 98";
+        assertEquals(true,TankRush.TankRush(3,4,S1,2,2,S2));
+    }
+
+    @Test
+    public void Sample2Test () {
+        String S1 = "3 4 1234 2345 0987";
+        String S2 = "2 2 32 79";
+        assertEquals(false,TankRush.TankRush(3,4,S1,2,2,S2));
+    }
+
+    @Test
+    public void MatrixThreeonThreeTest () {
+        String S1 = "4 5 92765 78018 23764 38612";
+        String S2 = "3 3 018 764 612";
+        assertEquals(true,TankRush.TankRush(4,5,S1,3,3,S2));
+    }
+
+
+    @Test
+    public void MatrixTwoOnTwoTest () {
+        String S1 = "4 5 92765 78018 23764 38612";
+        String S2 = "2 2 37 86";
+        assertEquals(true,TankRush.TankRush(4,5,S1,2,2,S2));
+    }
+
+    @Test
+    public void MatrixFourOnOneTest () {
+        String S1 = "4 5 92765 78018 23764 38612";
+        String S2 = "4 1 5 8 4 2";
+        assertEquals(true,TankRush.TankRush(4,5,S1,4,1,S2));
+    }
+
+    @Test
+    public void MatrixOneOnFiveTest () {
+        String S1 = "4 5 92765 78018 23764 38612";
+        String S2 = "1 5 23764";
+        assertEquals(true,TankRush.TankRush(4,5,S1,1,5,S2));
+    }
+
+    @Test
+    public void MatrixOneOnOneTest () {
+        String S1 = "4 5 92765 78038 23764 38632";
+        String S2 = "1 1 2";
+        assertEquals(true,TankRush.TankRush(4,5,S1,1,1,S2));
+    }
+
+}
