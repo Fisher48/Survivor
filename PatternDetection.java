776d0bf6f366192ebe5d153f9f98331ffbd3046f
Fisher48 <rybakov48@yandex.ru>
2024-04-12 22:24:39 +0300

    Added PatternDetection

---
+++ PatternDetection.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class PatternDetection {
+
+    public static boolean LineAnalysis(String line) {
+        String reverse = "";
+        if (line.charAt(0) != '*' || line.charAt(line.length()-1) != '*'){
+            return false;
+        }
+
+        for (int i = line.length()-1; i >= 0; i--) {
+            reverse += String.valueOf(line.charAt(i));
+        }
+
+        if (reverse.toString().equals(line)) {
+            return true;
+        }
+        return false;
+    }
+}
