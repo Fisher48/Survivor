fcc83ddc017f69b2af204307b733ff18e3fb5af8
Fisher48 <rybakov48@yandex.ru>
2024-04-15 12:23:47 +0300

    Added MisterRobot.java

---
+++ MisterRobot.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Level1 {
+
+    public static int[] func(int[] temp){
+        int x = 0;
+        for (int i = 0; i < 2; i++) {
+            x = temp[i];
+            temp[i] = temp[i + 1];
+            temp[i + 1] = x;
+        }
+        if (temp[0] <= temp[1] && temp[1] <= temp[2]) {
+            return temp;
+        } else
+            func(temp);
+        return temp;
+    }
+
+    public static boolean MisterRobot(int N, int[] data){
+        int[] temp = new int[3];
+        int[] sort = Arrays.copyOf(data, data.length);
+
+        boolean change = true;
+        for (int i = 0; i < N; i++) {
+            if (i < N - 2) {
+                if (data[i] < data[i + 1] && data[i + 1] > data[i + 2] && data[i + 2] < data[i]) {
+                    temp[0] = data[i];
+                    temp[1] = data[i + 1];
+                    temp[2] = data[i + 2];
+                    if (change) {
+                        func(temp);
+                        data[i] = temp[0];
+                        data[i + 1] = temp[1];
+                        data[i + 2] = temp[2];
+                        i -= i + 1;
+                    }
+                } else if (data[i] > data[i + 1] && data[i] < data[i + 2] && data[i + 1] < data[i + 2]) {
+                    return false;
+                }
+            }
+        }
+        Arrays.sort(sort);
+        return Arrays.equals(sort,data);
+    }
+}
+
+
