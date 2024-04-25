84616e60922bb4d85d1ac46303ce2ef336d11973
Fisher48 <rybakov48@yandex.ru>
2024-04-25 18:12:02 +0300

    Added MatrixTurnTest

---
+++ MatrixTurnTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MatrixTurnTest {
+
+    @Test
+    public void SampleTest() {
+        String[] matrix = {"123456", "234567", "345678", "456789"};
+        String[] matrixAfter = {"432123", "575434", "689645", "789876"};
+        String[] turnMatrix;
+        turnMatrix = matrix;
+        MatrixTurn.MatrixTurn(turnMatrix,4,6,3);
+        assertArrayEquals(matrixAfter,turnMatrix);
+    }
+
+    @Test
+    public void SquareMatrix() {
+        String[] matrix2 = {"1234", "2345", "3456", "4567"};
+        String[] matrixAfter = {"5432", "6341", "7452", "6543"};
+        String[] turnMatrix;
+        turnMatrix = matrix2;
+        MatrixTurn.MatrixTurn(turnMatrix,4,4,4);
+        assertArrayEquals(matrixAfter,turnMatrix);
+    }
+
+    @Test
+    public void BigMatrix() {
+        String[] matrix3 = {"1234567", "2345678", "3456789", "4567893", "1234567"};
+        String[] matrixAfter = {"3212345", "4543456", "1656767", "2789878", "3456739"};
+        String[] turnMatrix;
+        turnMatrix = matrix3;
+        MatrixTurn.MatrixTurn(turnMatrix,5,7,2);
+        assertArrayEquals(matrixAfter,turnMatrix);
+    }
+
+}
