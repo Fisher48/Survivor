156e328d52fe61f13e68db18dcb776de4eb0cbeb
Fisher48 <rybakov48@yandex.ru>
2024-04-26 08:34:46 +0300

    Added test from sample

--- ChubakaTransformTest.java
+++ ChubakaTransformTest.java
@@ -6,18 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ChubakaTransformTest {
 
     @Test
+    public void SampleTest(){
+
+        int[] a = {3,2,1};
+        int N = 3;
+        // sum = 58;
+        assertEquals(true,ChubakaTransform.TransformTransform(a,N));
+    }
+
+    @Test
     public void Pups(){
         int N = 7;
         int a[] = {1,7,6,4,3,2,5};
-        assertEquals(false,ChubakaTransform.TransformTransform(a,N));
+        assertEquals(true,ChubakaTransform.TransformTransform(a,N));
     }
 
-    @RepeatedTest(100)
-    public void SampleTest(){
+    @RepeatedTest(10)
+    public void RandomTests(){
         Random rand = new Random();
         int N = rand.nextInt(10)+1;
         int[] s = new int[N];
-        for (int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             s[i] = rand.nextInt(50)+1;
         }
         assertEquals(ChubakaTransform.TransformTransform(s,N),ChubakaTransform.TransformTransform(s,N));
