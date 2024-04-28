1395bb043cc5fe5f9530e02d5f4ef8bd9cc4ea38
Fisher48 <rybakov48@yandex.ru>
2024-04-28 19:41:40 +0300

    Added Football2

---
+++ Football2.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Football2 {
+
+    public static boolean Football(int F[], int N) {
+        boolean method = false;
+        int curr;
+        for (int i = N; i > 0; i--) {
+            if (F[i - 1] < F[i]) {
+                method = false;
+                break;
+            }
+            method = true;
+        }
+        if (!method) {
+            for (int i = 0; i < F.length - 1; i++) {
+                for (int j = i + 1; j < F.length; j++) {
+                    if (F[j] < F[i]) {
+                        curr = F[j];
+                        F[j] = F[i];
+                        F[i] = curr;
+                        for (int k = 0; k < N; k++) {
+                            if (F[k] > F[k + 1]) {
+                                curr = F[j];
+                                F[j] = F[i];
+                                F[i] = curr;
+                                method = false;
+                                break;
+                            }
+                            method = true;
+                        }
+                    }
+                    if (method) {
+                        break;
+                    }
+                }
+                if (method) {
+                    break;
+                }
+            }
+        }
+        return method;
+    }
+}
+
+
