8d5835f6ea53a46635d929a7abc65118cbab32a9
Fisher48 <rybakov48@yandex.ru>
2024-04-28 19:50:12 +0300

    Changed length in cycle

--- Football2.java
+++ Football2.java
@@ -5,7 +5,7 @@ public class Football2 {
     public static boolean Football(int F[], int N) {
         boolean method = false;
         int curr;
-        for (int i = N; i > 0; i--) {
+        for (int i = F.length-1; i > 0; i--) {
             if (F[i - 1] < F[i]) {
                 method = false;
                 break;
@@ -19,7 +19,7 @@ public class Football2 {
                         curr = F[j];
                         F[j] = F[i];
                         F[i] = curr;
-                        for (int k = 0; k < N; k++) {
+                        for (int k = 0; k < F.length-1; k++) {
                             if (F[k] > F[k + 1]) {
                                 curr = F[j];
                                 F[j] = F[i];
