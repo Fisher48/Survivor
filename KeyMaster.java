e1375c92606c0949b73fada19364d178a3e5d6f5
Fisher48 <rybakov48@yandex.ru>
2024-04-29 19:13:11 +0300

    Changed logic

--- KeyMaster.java
+++ KeyMaster.java
@@ -6,31 +6,16 @@ public class Level1 {
         StringBuffer doors = new StringBuffer();
         String[] matrixK = new String[k];
         Arrays.fill(matrixK, "0");
-
-        int steps = 0;
-        int count = 0;
-        for (int j = 0; j < k; j++) {
-            if (steps == 3) {
-                steps = 0;
-            }
-            steps++;
-            count = 0;
-            for (int i = 0; i < k; i++) {
-                count++;
-                if (steps == 1) {
+        for (int j = 1; j <= k; j++) {
+            for (int i = j - 1; i < k; i += j) {
+                if (matrixK[i].equals("0")) {
                     matrixK[i] = "1";
-                }
-                if (steps == 2 && count % 2 == 0) {
-                    matrixK[i] = "0";
-                }
-                if (steps == 3 && count % 3 == 0 && matrixK[i].equals("0")) {
-                    matrixK[i] = "1";
-                } else if (steps == 3 && count % 3 == 0 && matrixK[i].equals("1")) {
+                } else {
                     matrixK[i] = "0";
                 }
             }
         }
-        for (int x = 0; x < k; x++){
+        for (int x = 0; x < k; x++) {
             doors.append(matrixK[x]);
         }
         return String.valueOf(doors);
