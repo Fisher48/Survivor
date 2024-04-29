e13c7b7d6c7a3c181ccd57afa915b76dea358e99
Fisher48 <rybakov48@yandex.ru>
2024-04-29 19:13:37 +0300

    Changed logic

--- KeyMasterTest.java
+++ KeyMasterTest.java
@@ -17,39 +17,43 @@ public class KeyMasterTest {
     }
     @Test
     public void FourDoors(){
-        assertEquals("1111",KeyMaster.Keymaker(4));
+        assertEquals("1001",KeyMaster.Keymaker(4));
     }
     @Test
     public void FiveDoors(){
-        assertEquals("10101",KeyMaster.Keymaker(5));
+        assertEquals("10010",KeyMaster.Keymaker(5));
     }
     @Test
     public void SixDoors(){
-        assertEquals("100011",KeyMaster.Keymaker(6));
+        assertEquals("100100",KeyMaster.Keymaker(6));
     }
     @Test
     public void SevenDoors(){
-        assertEquals("1111111",KeyMaster.Keymaker(7));
+        assertEquals("1001000",KeyMaster.Keymaker(7));
     }
     @Test
     public void EigthDoors(){
-        assertEquals("10101010",KeyMaster.Keymaker(8));
+        assertEquals("10010000",KeyMaster.Keymaker(8));
     }
     @Test
     public void NineDoors(){
-        assertEquals("100011100",KeyMaster.Keymaker(9));
+        assertEquals("100100001",KeyMaster.Keymaker(9));
     }
     @Test
     public void TenDoors(){
-        assertEquals("1111111111",KeyMaster.Keymaker(10));
+        assertEquals("1001000010",KeyMaster.Keymaker(10));
     }
     @Test
     public void ElevenDoors(){
-        assertEquals("10101010101",KeyMaster.Keymaker(11));
+        assertEquals("10010000100",KeyMaster.Keymaker(11));
     }
     @Test
     public void TwelveDoors(){
-        assertEquals("100011100011",KeyMaster.Keymaker(12));
+        assertEquals("100100001000",KeyMaster.Keymaker(12));
+    }
+    @Test
+    public void FifteenDoors(){
+        assertEquals("10010000100000010000000010000000000100000000000010",KeyMaster.Keymaker(50));
     }
 
 }
