35c85433fe896a83fb4fcec5e721692d7882d925
Fisher48 <rybakov48@yandex.ru>
2024-05-01 22:43:06 +0300

    Added ExponentiationTest

---
+++ ExponentiationTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExponentiationTest {
+
+    @Test
+    public void expTwoInFive(){
+        assertEquals(32,Exponentiation.expon(2,5));
+    }
+
+    @Test
+    public void expFiveInSix(){
+        assertEquals(15625,Exponentiation.expon(5,6));
+    }
+
+    @Test
+    public void SumOfThe6Digits(){
+        assertEquals(26,Exponentiation.sumOfTheDigits(278621));
+    }
+
+    @Test
+    public void SumOfThe5Digits(){
+        assertEquals(14,Exponentiation.sumOfTheDigits(13172));
+    }
+
+    @Test
+    public void SumOfThe9Digits(){
+        assertEquals(16,Exponentiation.sumOfTheDigits(211023412));
+    }
+
+}
