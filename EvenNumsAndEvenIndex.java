0195f45ec39c5a248238fdb9a4114d4cf1379611
Fisher48 <rybakov48@yandex.ru>
2024-05-07 08:12:15 +0300

    Exclude adding in additional List

--- EvenNumsAndEvenIndex.java
+++ EvenNumsAndEvenIndex.java
@@ -4,36 +4,26 @@ public class EvenNumsAndEvenIndex {
 
     // Печать только четных значений из списка
     public static List<Integer> evenNums(List<Integer> list) {
-        List<Integer> nums = new ArrayList<>();
         if (list.isEmpty()) {
-            return nums;
+            return null;
         }
-        nums.add(list.getLast());
-        list.remove(list.getLast());
-        evenNums(list);
-        if (nums.getLast() % 2 == 0) {
-            System.out.println(nums.getLast());
-            list.add(nums.getLast());
+        if (list.getFirst() % 2 == 0) {
+            System.out.println(list.getFirst());
         }
-        return list;
+        list.remove(list.getFirst());
+        return evenNums(list);
     }
 
     // Печать элементов списка с чётными индексами;
     public static List<String> evenIndex(List<String> list) {
-        List<String> index = new ArrayList<>();
-        int count = list.size();
         if (list.isEmpty()) {
-            return index;
+            return null;
         }
-        index.add(list.getLast());
+        if ((list.size()-1) % 2 == 0) {
+            System.out.println(list.getLast());
+        }
         list.remove(list.getLast());
-        count--;
-        evenIndex(list);
-        if (count % 2 == 0) {
-            System.out.println(index.getLast());
-            list.add(index.getLast());
-        }
-        return list;
+        return evenIndex(list);
     }
 }
 
