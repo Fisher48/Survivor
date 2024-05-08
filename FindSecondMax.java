b500e53fb420a2eed13ba45bed7b65cf206cf4c9
Fisher48 <rybakov48@yandex.ru>
2024-05-08 09:33:34 +0300

    Changed approach in functions without sorting are list

--- FindSecondMax.java
+++ FindSecondMax.java
@@ -2,38 +2,36 @@ import java.util.*;
 
 public class FindSecondMax {
 
-    public static boolean check(List<Integer> matrix) {
-        Integer[] arr = matrix.toArray(new Integer[0]);
-        Arrays.sort(arr, Collections.reverseOrder());
-        List<Integer> list = new ArrayList<>(List.of(arr));
-        return list.equals(matrix);
-    }
 
-    public static int SecMax(List<Integer> arr, int count) {
-        int max = 0;
-        int temp = 0;
-        if (count == arr.size()-1) {
-            count = 0;
+    public static int SecMax(List<Integer> nums, int max1, int max2, int index) {
+        if (index > nums.size() - 1) {
+            return max2;
         }
-        if (arr.get(count) <= arr.get(count+1)) {
-            temp = arr.get(count);
-            max = arr.get(count+1);
-            arr.set(count,max);
-            arr.set(count+1,temp);
+        if (nums.get(index) >= max1 && nums.get(index) >= max2) {
+            max2 = max1;
+            max1 = nums.get(index);
         }
-        count++;
-        if (check(arr)) {
-            return arr.get(1);
+        else if (nums.get(index) > max2 && nums.get(index) <= max1) {
+            max2 = nums.get(index);
         }
-        return SecMax(arr, count);
+        return SecMax(nums, max1, max2, index + 1);
     }
 
     public static int findSecMax(List<Integer> numbs) {
-        if (numbs.size() == 1) {
+        if (numbs.size() <= 1) {
             return numbs.getFirst();
         }
-        return SecMax(numbs,0);
+        int max1 = 0; int max2 = 0;
+        if (numbs.get(0) > numbs.get(1)) {
+            max1 = numbs.get(0);
+            max2 = numbs.get(1);
+        } else {
+            max2 = numbs.get(0);
+            max1 = numbs.get(1);
+        }
+        return SecMax(numbs, max1, max2, 2);
     }
+
 }
 
 
