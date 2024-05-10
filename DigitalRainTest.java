56d605a00d53d6281453f03aeae2daebf6ad9dfb
Fisher48 <rybakov48@yandex.ru>
2024-05-10 22:15:27 +0300

    Changed tests

--- DigitalRainTest.java
+++ DigitalRainTest.java
@@ -5,14 +5,14 @@ public class DigitalRainTest {
 
     @Test
     public void SampleTest() {
-        assertEquals("10",DigitalRain.digital_rain("1111011"));
-        assertEquals("10",DigitalRain.digital_rain("1011111"));
+        assertEquals("01",DigitalRain.digital_rain("1111011"));
+        assertEquals("01",DigitalRain.digital_rain("1011111"));
         assertEquals("111000",DigitalRain.digital_rain("1111000"));
         assertEquals("11101000",DigitalRain.digital_rain("11101000"));
         assertEquals("",DigitalRain.digital_rain("11111111"));
         assertEquals("10",DigitalRain.digital_rain("011111110"));
         assertEquals("110000011110",DigitalRain.digital_rain("0110000011110"));
-        assertEquals("",DigitalRain.digital_rain("0000000001"));
+        assertEquals("01",DigitalRain.digital_rain("0000000001"));
     }
 
 }
