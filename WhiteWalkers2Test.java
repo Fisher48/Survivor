b0cbfb8b79cd183091beab646f4d2dda01ad1db3
Fisher48 <rybakov48@yandex.ru>
2024-05-10 21:58:24 +0300

    Added test for Reference

--- WhiteWalkers2Test.java
+++ WhiteWalkers2Test.java
@@ -15,4 +15,16 @@ public class WhiteWalkers2Test {
         assertEquals(true,WhiteWalkers2.walkFind("6fg==o=oo4oooo=5=4"));
     }
 
+    @Test
+    public void TestForReference(){
+        assertEquals(true,WhiteWalkersReference.findWlakers("axxb6===4xaf5===eee5"));
+        assertEquals(false,WhiteWalkersReference.findWlakers("5==ooooooo=5=5"));
+        assertEquals(true,WhiteWalkersReference.findWlakers("abc=7==hdjs=3gg1=======5"));
+        assertEquals(false,WhiteWalkersReference.findWlakers("aaS=8"));
+        assertEquals(true,WhiteWalkersReference.findWlakers("9===1===9===1===9"));
+        assertEquals(true,WhiteWalkersReference.findWlakers("ad3===4sd5===5ds"));
+        assertEquals(false,WhiteWalkersReference.findWlakers("6fg==o=oo3oooo=5=4"));
+        assertEquals(true,WhiteWalkersReference.findWlakers("6fg==o=oo4oooo=5=4"));
+    }
+
 }
