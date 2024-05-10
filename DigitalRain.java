3d3d702da4238106347f854c5e7cffb7663d408f
Fisher48 <rybakov48@yandex.ru>
2024-05-10 21:56:10 +0300

    Created DigitalRain

---
+++ DigitalRain.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class DigitalRain {
+
+    public static String digital_rain(String col){
+        StringBuffer sb = new StringBuffer();
+        String temp = col;
+        int one = 0;
+        int zero = 0;
+        List<String> maxSubstring = new ArrayList<>();
+        for (int i = 0; i < temp.length(); i++) {
+                if (temp.charAt(i) == '1') {
+                    one++;
+                    sb.append(temp.charAt(i));
+                }
+                if (temp.charAt(i) == '0') {
+                    zero++;
+                    sb.append(temp.charAt(i));
+                }
+                if (i == temp.length() - 1 && one != zero) {
+                    i = -1;
+                    zero = 0;
+                    one = 0;
+                    sb.deleteCharAt(0);
+                    temp = String.valueOf(sb);
+                    sb.delete(0, sb.length());
+                } else if (one == zero) {
+                    if (sb.charAt(0) == '0') {
+                        continue;
+                    }
+                    maxSubstring.add(String.valueOf(sb));
+                }
+        }
+        if (maxSubstring.isEmpty()) {
+            return "";
+        } else {
+            return maxSubstring.getLast();
+        }
+    }
+}
+
+
