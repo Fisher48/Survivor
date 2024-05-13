f3a962ff29f9051f2f497a9d277dbe06527abc37
Fisher48 <rybakov48@yandex.ru>
2024-05-13 20:46:43 +0300

    Changed Tests for differ approach

--- DigitalRainTest.java
+++ DigitalRainTest.java
@@ -5,14 +5,15 @@ public class DigitalRainTest {
 
     @Test
     public void SampleTest() {
-        assertEquals("01",DigitalRain.digital_rain("1111011"));
-        assertEquals("01",DigitalRain.digital_rain("1011111"));
-        assertEquals("111000",DigitalRain.digital_rain("1111000"));
-        assertEquals("11101000",DigitalRain.digital_rain("11101000"));
-        assertEquals("",DigitalRain.digital_rain("11111111"));
-        assertEquals("10",DigitalRain.digital_rain("011111110"));
-        assertEquals("110000011110",DigitalRain.digital_rain("0110000011110"));
-        assertEquals("01",DigitalRain.digital_rain("0000000001"));
+        assertEquals("10001110", DigitalRain2.digital_rain("110001110"));
+        assertEquals("0011", DigitalRain2.digital_rain("1111001111"));
+        assertEquals("01", DigitalRain2.digital_rain("10111011"));
+        assertEquals("111000", DigitalRain2.digital_rain("1111000"));
+        assertEquals("11101000", DigitalRain2.digital_rain("11101000"));
+        assertEquals("", DigitalRain2.digital_rain("11111111"));
+        assertEquals("10", DigitalRain2.digital_rain("011111110"));
+        assertEquals("110000011110", DigitalRain2.digital_rain("0110000011110"));
+        assertEquals("01", DigitalRain2.digital_rain("0000000001"));
     }
 
 }
