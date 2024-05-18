5be61c335ac7fb4d6f3b8a53220f9ac6e6a7771b
Fisher48 <rybakov48@yandex.ru>
2024-05-18 12:10:53 +0300

    Created MuscleFibers

---
+++ MuscleFibers.java
@@ -0,0 +1,22 @@
+public class MuscleFibers {
+
+    public static int artificial_muscle_fibers(int[] arr) {
+
+        byte[] buffer = new byte[8192];
+        int duplicates = 0;
+        for (int num : arr) {
+            int index = num / 8;
+            int bit = num % 8;
+            if ((buffer[index] & (1 << bit)) == 0) {
+                buffer[index] |= (byte) (1 << bit);
+            }
+            else if ((buffer[index + 4096] & (1 << bit)) == 0) {
+                    buffer[index + 4096] |= (byte) (1 << bit);
+                    duplicates++;
+            }
+        }
+        return duplicates;
+    }
+}
+
+
