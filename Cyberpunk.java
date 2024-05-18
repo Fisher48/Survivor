217a1ff3c4c14c3596dc4728c31e6fbf035f89cd
Fisher48 <rybakov48@yandex.ru>
2024-05-18 12:22:22 +0300

    Improving code after reference

--- Cyberpunk.java
+++ Cyberpunk.java
@@ -9,11 +9,12 @@ public class Cyberpunk {
         Map<Integer, Integer> repeatList = new HashMap<>();
         for (int i = 0; i < arr1.length; i++) {
             repeatList.put(arr1[i], repeatList.getOrDefault(arr1[i],0) + 1);
-            repeatList.put(arr2[i], repeatList.getOrDefault(arr2[i],0) - 1);
         }
-        for (Integer j : repeatList.values()) {
-            if (j != 0) {
+        for (Integer j : arr2) {
+            if (!repeatList.containsKey(j) || repeatList.get(j) == 0) {
                 return false;
+            } else {
+                repeatList.put(j, repeatList.getOrDefault(j,0) - 1);
             }
         }
         return true;
