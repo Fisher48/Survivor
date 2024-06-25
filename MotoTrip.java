8bb4326f90aeaf461ddce271cebc5d21ce35e579
Fisher48 <rybakov48@yandex.ru>
2024-06-25 15:48:01 +0300

    Changed names

--- MotoTrip.java
+++ MotoTrip.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class MotoTrip {
 
-    public static int getTotalDistance(int[] oksana) {
-        int totalDistance = 0;
+    public static int getTotalDistanceKm(int[] oksana) {
+        int totalDistanceKm = 0;
         if (oksana.length < 2) {
             throw new IllegalArgumentException("Array size should be >= 2: " + "but current size: " + oksana.length);
         }
@@ -12,12 +12,12 @@ public class MotoTrip {
                 throw new IllegalArgumentException("Speed or time is negative: " + oksana[i]);
             }
             if (i % 2 == 0 && i > 0) {
-                totalDistance += oksana[i] * (oksana[i + 1] - oksana[i - 1]);
+                totalDistanceKm += oksana[i] * (oksana[i + 1] - oksana[i - 1]);
             } else if (i == 0) {
-                totalDistance = oksana[i] * oksana[i + 1];
+                totalDistanceKm = oksana[i] * oksana[i + 1];
             }
         }
-        return totalDistance;
+        return totalDistanceKm;
     }
 }
 
--- MotoTripTest.java
+++ MotoTripTest.java
@@ -6,41 +6,41 @@ public class MotoTripTest {
     @Test
     public void SampleTest () {
         int a[] = {10,1,20,2};
-        assertEquals(30, MotoTrip.getTotalDistance(a));
+        assertEquals(30, MotoTrip.getTotalDistanceKm(a));
     }
 
     @Test
     public void OneNumbTest () {
         int a[] = {1,1,1,1,1,1};
-        assertEquals(1, MotoTrip.getTotalDistance(a));
+        assertEquals(1, MotoTrip.getTotalDistanceKm(a));
     }
 
     @Test
     public void AccelerationTest () {
         int a[] = {60,3,70,5,100,6};
-        assertEquals(420, MotoTrip.getTotalDistance(a));
+        assertEquals(420, MotoTrip.getTotalDistanceKm(a));
     }
 
     @Test
     public void ContinueTest () {
         int a[] = {60,3,70,5,70,8};
-        assertEquals(530, MotoTrip.getTotalDistance(a));
+        assertEquals(530, MotoTrip.getTotalDistanceKm(a));
     }
 
     @Test
     public void ZeroTest () {
         int a[] = {0,0};
-        assertEquals(0, MotoTrip.getTotalDistance(a));
+        assertEquals(0, MotoTrip.getTotalDistanceKm(a));
     }
     @Test
     public void RepeatTest () {
         int a[] = {20,2};
-        assertEquals(40, MotoTrip.getTotalDistance(a));
+        assertEquals(40, MotoTrip.getTotalDistanceKm(a));
     }
 
     @Test
     public void WrongTest () {
         int a[] = {20,2,30,6,10,7};
-        assertEquals(170, MotoTrip.getTotalDistance(a));
+        assertEquals(170, MotoTrip.getTotalDistanceKm(a));
     }
 }
