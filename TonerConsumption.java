49d7c4e5a39cc6906d6bf346b570c03e5645a731
Fisher48 <rybakov48@yandex.ru>
2024-06-28 14:15:15 +0300

    Changed on Map and added for-each

--- TonerConsumption.java
+++ TonerConsumption.java
@@ -5,144 +5,45 @@ public class TonerConsumption {
     public static final int DEFAULT_CONSUMPTION = 23;
 
     public static int getConsumption(String Line) {
+        Map<Character, Integer> costBySymbols = new HashMap<>();
+        costBySymbols.put(' ',0);
+        costBySymbols.put('%',22); costBySymbols.put('2',22); costBySymbols.put('m',22); costBySymbols.put('0',22); costBySymbols.put('Z',22);
+        costBySymbols.put('&',24); costBySymbols.put('#',24); costBySymbols.put('A',24); costBySymbols.put('y',24);
+        costBySymbols.put('+',13); costBySymbols.put('v',13); costBySymbols.put('x',13); costBySymbols.put('r',13);
+        costBySymbols.put(',',7); costBySymbols.put('-',7); costBySymbols.put('^',7);
+        costBySymbols.put('1',19); costBySymbols.put('V',19); costBySymbols.put('z',19); costBySymbols.put('w',19);
+        costBySymbols.put('7',16); costBySymbols.put('L',16); costBySymbols.put('T',16); costBySymbols.put('l',16);
+        costBySymbols.put('8',23); costBySymbols.put('P',23); costBySymbols.put('U',23); costBySymbols.put('a',23); costBySymbols.put('3',23); costBySymbols.put('e',23);
+        costBySymbols.put('=',14); costBySymbols.put('Y',14);
+        costBySymbols.put('>',10); costBySymbols.put('\\',10); costBySymbols.put('/',10); costBySymbols.put('<',10);
+        costBySymbols.put('C',20); costBySymbols.put('o',20); costBySymbols.put('j',20); costBySymbols.put('F',20);
+        costBySymbols.put('D',26); costBySymbols.put('E',26); costBySymbols.put('O',26);
+        costBySymbols.put('9',26); costBySymbols.put('W',26); costBySymbols.put('6',26);
+        costBySymbols.put('I',18); costBySymbols.put('J',18); costBySymbols.put('[',18); costBySymbols.put(']',18);
+        costBySymbols.put('n',18); costBySymbols.put('X',18); costBySymbols.put('f',18); costBySymbols.put('{',18); costBySymbols.put('}',18);
+        costBySymbols.put('b',25); costBySymbols.put('d',25); costBySymbols.put('p',25); costBySymbols.put('q',25);
+        costBySymbols.put('G',25); costBySymbols.put('S',25); costBySymbols.put('H',25); costBySymbols.put('N',25);
+        costBySymbols.put('g',30);
+        costBySymbols.put('h',21); costBySymbols.put('s',21); costBySymbols.put('K',21); costBySymbols.put('4',21); costBySymbols.put('k',21);
+        costBySymbols.put('t',17); costBySymbols.put('c',17); costBySymbols.put('u',17); costBySymbols.put('*',17);
+        costBySymbols.put('!',9); costBySymbols.put('~',9);
+        costBySymbols.put('\'',3); costBySymbols.put('`',3);
+        costBySymbols.put('?',15); costBySymbols.put('i',15);
+        costBySymbols.put('Q',31);
+        costBySymbols.put('"',6);
+        costBySymbols.put('(',12); costBySymbols.put(')',12); costBySymbols.put('|',12);
+        costBySymbols.put('.',4);
+        costBySymbols.put(':',8);
+        costBySymbols.put('@',32);
+        costBySymbols.put('R',28); costBySymbols.put('M',28);
+        costBySymbols.put('5',27);
+        costBySymbols.put(';',11);
+        costBySymbols.put('_',8);
+        costBySymbols.put('$',29); costBySymbols.put('B',29);
+
         int sumTonerConsumption = 0;
-        for (int i = 0; i < Line.length(); i++) {
-            if (Line.charAt(i) == ' ') {
-                sumTonerConsumption += 0;
-                continue;
-            }
-            if (Line.charAt(i) == '%' || Line.charAt(i) == '2' || Line.charAt(i) == 'm' || Line.charAt(i) == '0'
-                    || Line.charAt(i) == 'Z'){
-                sumTonerConsumption += 22;
-                continue;
-            }
-            if (Line.charAt(i) == '&' || Line.charAt(i) == '#' || Line.charAt(i) == 'A' || Line.charAt(i) == 'y') {
-                sumTonerConsumption += 24;
-                continue;
-            }
-            if (Line.charAt(i) == '+' || Line.charAt(i) == 'v' || Line.charAt(i) == 'x' || Line.charAt(i) == 'r') {
-                sumTonerConsumption += 13;
-                continue;
-            }
-            if (Line.charAt(i) == ',' || Line.charAt(i) == '-' || Line.charAt(i) == '^') {
-                sumTonerConsumption += 7;
-                continue;
-            }
-            if (Line.charAt(i) == '1' || Line.charAt(i) == 'V' || Line.charAt(i) == 'z' || Line.charAt(i) == 'w') {
-                sumTonerConsumption += 19;
-                continue;
-            }
-            if (Line.charAt(i) == '7' || Line.charAt(i) == 'L' || Line.charAt(i) == 'T' || Line.charAt(i) == 'l') {
-                sumTonerConsumption += 16;
-                continue;
-            }
-            if (Line.charAt(i) == '8' || Line.charAt(i) == 'P' || Line.charAt(i) == 'U' || Line.charAt(i) == 'a'
-                    || Line.charAt(i) == '3' || Line.charAt(i) == 'e') {
-                sumTonerConsumption += 23;
-                continue;
-            }
-            if (Line.charAt(i) == '=' || Line.charAt(i) == 'Y') {
-                sumTonerConsumption += 14;
-                continue;
-            }
-            if (Line.charAt(i) == '>' || Line.charAt(i) == '\\' || Line.charAt(i) == '/' || Line.charAt(i) == '<') {
-                sumTonerConsumption += 10;
-                continue;
-            }
-            if (Line.charAt(i) == 'C' || Line.charAt(i) == 'o' || Line.charAt(i) == 'j' || Line.charAt(i) == 'F') {
-                sumTonerConsumption += 20;
-                continue;
-            }
-            if (Line.charAt(i) == 'D' || Line.charAt(i) == 'E' || Line.charAt(i) == 'O'
-                    || Line.charAt(i) == '9' || Line.charAt(i) == 'W' || Line.charAt(i) == '6') {
-                sumTonerConsumption += 26;
-                continue;
-            }
-            if (Line.charAt(i) == 'I' || Line.charAt(i) == 'J' || Line.charAt(i) == '[' || Line.charAt(i) == ']'
-                    || Line.charAt(i) == 'n' || Line.charAt(i) == 'X' || Line.charAt(i) == 'f'
-                    || Line.charAt(i) == '{' || Line.charAt(i) == '}') {
-                sumTonerConsumption += 18;
-                continue;
-            }
-            if (Line.charAt(i) == 'b' || Line.charAt(i) == 'd' || Line.charAt(i) == 'p' || Line.charAt(i) == 'q'
-                    || Line.charAt(i) == 'G' || Line.charAt(i) == 'S' || Line.charAt(i) == 'H' || Line.charAt(i) == 'N') {
-                sumTonerConsumption += 25;
-                continue;
-            }
-            if (Line.charAt(i) == 'g') {
-                sumTonerConsumption += 30;
-                continue;
-            }
-            if (Line.charAt(i) == 'h' || Line.charAt(i) == 's' || Line.charAt(i) == 'K'
-                    || Line.charAt(i) == '4' || Line.charAt(i) == 'k') {
-                sumTonerConsumption += 21;
-                continue;
-            }
-            if (Line.charAt(i) == 't' || Line.charAt(i) == 'c' || Line.charAt(i) == 'u' || Line.charAt(i) == '*') {
-                sumTonerConsumption += 17;
-                continue;
-            }
-            if (Line.charAt(i) == '!' || Line.charAt(i) == '~') {
-                sumTonerConsumption += 9;
-                continue;
-            }
-            if (Line.charAt(i) == '\'' || Line.charAt(i) == '`') {
-                sumTonerConsumption += 3;
-                continue;
-            }
-            if (Line.charAt(i) == '?' || Line.charAt(i) == 'i') {
-                sumTonerConsumption += 15;
-                continue;
-            }
-            if (Line.charAt(i) == 'Q') {
-                sumTonerConsumption += 31;
-                continue;
-            }
-            if (Line.charAt(i) == '"') {
-                sumTonerConsumption += 6;
-                continue;
-            }
-            if (Line.charAt(i) == '(' || Line.charAt(i) == ')' || Line.charAt(i) == '|'){
-                sumTonerConsumption += 12;
-                continue;
-            }
-            if (Line.charAt(i) == '.') {
-                sumTonerConsumption += 4;
-                continue;
-            }
-            if (Line.charAt(i) == ':') {
-                sumTonerConsumption += 8;
-                continue;
-            }
-            if (Line.charAt(i) == '@') {
-                sumTonerConsumption += 32;
-                continue;
-            }
-            if (Line.charAt(i) == 'R' || Line.charAt(i) == 'M') {
-                sumTonerConsumption += 28;
-                continue;
-            }
-            if (Line.charAt(i) == '5') {
-                sumTonerConsumption += 27;
-                continue;
-            }
-            if (Line.charAt(i) == ';') {
-                sumTonerConsumption += 11;
-                continue;
-            }
-            if (Line.charAt(i) == '_') {
-                sumTonerConsumption += 8;
-                continue;
-            }
-            if (Line.charAt(i) == '$' || Line.charAt(i) == 'B') {
-                sumTonerConsumption += 29;
-                continue;
-            }
-            if (Line.charAt(i) == '6') {
-                sumTonerConsumption += 26;
-            } else {
-                sumTonerConsumption += DEFAULT_CONSUMPTION;
-            }
+        for (char symbol : Line.toCharArray()) {
+            sumTonerConsumption += costBySymbols.getOrDefault(symbol, DEFAULT_CONSUMPTION);
         }
         return sumTonerConsumption;
     }
